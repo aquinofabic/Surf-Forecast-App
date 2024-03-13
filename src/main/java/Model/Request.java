77d018dd893841c61ecf65b885e9ff79360f970d
Dale Fabic <130316665+aquinofabic@users.noreply.github.com>
2024-03-13 21:38:42 +0000

    Added functionality to print swell height in feet at each hour of the day in Sagres.

--- src/main/java/Model/Request.java
+++ src/main/java/Model/Request.java
@@ -23,19 +23,28 @@ public class Request {
         // calls response body (all data in list)
         JSONObject object = new JSONObject(response.body());
 
-        // calls forecastday from the response body (second object list)
+        // calls forecastday from the response body
         JSONArray forecastday = object.getJSONObject("forecast").getJSONArray("forecastday");
 
-        // calls hour from forecast list (third object in list)
+        // calls hour from forecast list
+        // index represents 24 lists corresponding to data at each hour of the day
         JSONArray hour = forecastday.getJSONObject(0).getJSONArray("hour");
 
         // Prints swell height in feet!
-        float swell_ht_ft = hour.getJSONObject(0).getFloat("swell_ht_ft");
-        System.out.println(swell_ht_ft);
+        /*float swell_ht_ft = hour.getJSONObject(0).getFloat("swell_ht_ft");
+        System.out.println(swell_ht_ft);*/
 
-        float swell_ht_test = hour.getJSONObject(1).getFloat("swell_ht_ft");
-        System.out.println(swell_ht_test);
+        /*float swell_ht_test = hour.getJSONObject(1).getFloat("swell_ht_ft");
+        System.out.println(swell_ht_test);*/
         // the index 0, 1 are the times. 00:00, 01:00, need to fix that it calls all the times.
+
+
+        // Prints swell height in feet at each hour of the day.
+        int i = 0;
+        for (hour.getJSONObject(i); i < hour.length(); i++) {
+            float swellHtFt=  hour.getJSONObject(i).getFloat("swell_ht_ft");
+            System.out.println(swellHtFt);
+        }
 
 /*
         System.out.println(response.body());
